c82885b68c57088418ad8492d9ffa725cd4d96cf
Aleksandr <skon1207@yandex.ru>
2024-04-16 02:05:43 +0400

    feat: add recursive function fib(n)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int fib(int n){
+    if (n == 1 || n == 2)
+        return 1;
+    return fib(n - 1) + fib(n - 2);
+}
 int main(){
-    cout << "Hello, world!";
+    int n;
+    cout << "n = "; cin >> n;
+    cout << "result: " << fib(n) << endl;
     return 0;
 }

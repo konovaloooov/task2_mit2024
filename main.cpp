af31c183fa39eeb4471908e7c6bd940fbe0543c9
Aleksandr <skon1207@yandex.ru>
2024-04-16 02:43:22 +0400

    refactor: iteratively computing the sequence starting from 0,1...

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n){
-    if (n == 1 || n == 2)
-        return n - 1;
-    return fib(n - 1) + fib(n - 2);
+void fib(int n){
+    int tmp1 = 0, tmp2 = 1, tmp3;
+    if (n == 1){
+        cout << tmp1 << endl;
+        return;
+    }
+    cout << tmp1 << endl << tmp2 << endl;
+
+    for(int i = 2; i < n; i++){
+        tmp3 = tmp1 + tmp2;
+        cout << tmp3 << endl;
+        tmp1 = tmp2;
+        tmp2 = tmp3;
+    } 
 }
+
 int main(){
     int n;
     cout << "n = "; cin >> n;
-    cout << "result: " << fib(n) << endl;
+    cout << "result:" << endl;
+    fib(n);
     return 0;
 }
